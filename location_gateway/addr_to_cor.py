b554ace3eab71083c2eecf3db42743ab2ea9cc1b
woodprogrammer <asddeneme3@gmail.com>
2017-11-16 16:09:11 +0200

    Zomato apiadded

--- location_gateway/addr_to_cor.py
+++ location_gateway/addr_to_cor.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route('/addr_to_co', methods=['GET'])
-def get_tasks():
+def get_geo():
     
         addr_location = request.args.get("my_addr")
         data = requests.get("http://maps.google.com/maps/api/geocode/json?address={}".format(addr_location))
@@ -13,6 +13,9 @@ def get_tasks():
             return jsonify(lat=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lat"],long=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lng"])
         except:
             return jsonify(status=False)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
