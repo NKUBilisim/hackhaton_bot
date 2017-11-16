497db3259c4983d87ca8f0c1d84e968e3c1b4d28
utkucanbykl <utkucanbykl@gmail.com>
2017-11-16 14:24:05 +0300

    First Commit Today

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = "486717629:AAET2FKjId2ehdhUeK-UYjQqkbtW35Musds"
 updater = Updater(token=TOKEN)
@@ -10,13 +10,15 @@ dispatcher = updater.dispatcher
 
 def Start(bot, update):
     location_keyboard = telegram.KeyboardButton(text="send_location", request_location=True)
-    contact_keyboard = telegram.KeyboardButton(text="send_contact", request_contact=True)
+    contact_keyboard = telegram.KeyboardButton(text="/send_contact", request_contact=True)
     custom_keyboard = [[location_keyboard, contact_keyboard]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     bot.sendMessage(chat_id=update.message.chat_id, text = "Would you mind sharing your location and contact with me?",
     reply_markup = reply_markup)
+    update.message.reply_text('Hello World!')
 
-
+def get_location(bot, update):
+    print(update.message.location)
 
 ##Handler
 
@@ -28,4 +30,8 @@ start_handler = CommandHandler("start", Start)
 
 dispatcher.add_handler(start_handler)
 
+
+
+
+dispatcher.add_handler(MessageHandler(Filters.location, get_location))
 updater.start_polling()
