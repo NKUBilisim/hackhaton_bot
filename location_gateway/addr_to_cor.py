02eaacc76a1d7c29b958a876186240755f9d5fe2
woodprogrammer <asddeneme3@gmail.com>
2017-11-16 15:24:45 +0200

    Bug Fix

--- location_gateway/addr_to_cor.py
+++ location_gateway/addr_to_cor.py
@@ -8,12 +8,11 @@ def get_tasks():
     
         addr_location = request.args.get("my_addr")
         data = requests.get("http://maps.google.com/maps/api/geocode/json?address={}".format(addr_location))
-        tmp_data = json.loads(data.content) 
+        tmp_data = json.loads(data.content)
         try:
-            return jsonify(lat=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lat"],long=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lon"])
+            return jsonify(lat=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lat"],long=tmp_data["results"][0]["geometry"]["bounds"]["northeast"]["lng"])
         except:
             return jsonify(status=False)
-        
 if __name__ == '__main__':
     app.run(debug=True)
 
